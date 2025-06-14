230652cb96865ceec63cd2adb91b63a42ef21acb
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-14 11:27:40 +0530

    Created full CRUD functionality for Dish

--- controller/dishController.js
+++ controller/dishController.js
@@ -1,8 +1,11 @@
+import { eq, lte } from "drizzle-orm";
 import db from "../config/db.js";
 import { Dish } from "../drizzle/schema.js";
 
 export const createDish = async (req, res) => {
   try {
+    const newDish = await db.insert(Dish).values(req.body);
+    return res.status(201).json(newDish);
   } catch (err) {
     return res.status(400).send("Something Went Wrong");
   }
@@ -10,7 +13,23 @@ export const createDish = async (req, res) => {
 
 export const getDish = async (req, res) => {
   try {
-    res.send("dish");
+    const { name, price } = req.query;
+
+    if (name) {
+      const dishData = await db.select().from(Dish).where(eq(Dish.name, name));
+      return res.status(200).json(dishData);
+    }
+
+    if (price) {
+      const dishData = await db
+        .select()
+        .from(Dish)
+        .where(lte(Dish.price, price));
+      return res.status(200).json(dishData);
+    }
+
+    const dishData = await db.select().from(Dish);
+    return res.status(200).json(dishData);
   } catch (err) {
     return res.status(400).send("Something Went Wrong");
   }
@@ -18,6 +37,11 @@ export const getDish = async (req, res) => {
 
 export const updateDish = async (req, res) => {
   try {
+    const updatedDish = await db
+      .update(Dish)
+      .set(req.body)
+      .where(eq(Dish.id, req.params.id));
+    return res.status(200).json(updatedDish);
   } catch (err) {
     return res.status(400).send("Something Went Wrong");
   }
@@ -25,6 +49,8 @@ export const updateDish = async (req, res) => {
 
 export const deleteDish = async (req, res) => {
   try {
+    const deletedDish = await db.delete(Dish).where(eq(Dish.id, req.params.id));
+    return res.status(200).json(deletedDish);
   } catch (err) {
     return res.status(400).send("Something Went Wrong");
   }
